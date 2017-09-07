c84a2f66fa8efdfc18d6b897383efb92c8ae2906
Mammothrider <xushubin1992@gmail.com>
2017-09-07 18:31:19 +0900

    Character Move, Camera Follow

---
+++ BodyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyController : MonoBehaviour {
+    
+    public float speed = 1;
+    private Rigidbody2D selfRigidbody;
+	// Update is called once per frame
+    void Awake() {
+        selfRigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    protected void MoveTowards(Vector3 direction) {
+        selfRigidbody.MovePosition(transform.position + direction);
+    }
+    
+    protected void AimAt(Vector3 direction) {
+        Vector3 targetDirection = direction.normalized;
+        // Debug.DrawRay(transform.position, targetDirection, Color.red);
+        // Debug.DrawRay(transform.position, Vector3.up, Color.blue);
+        // Debug.DrawRay(transform.position, Vector3.right, Color.green);
+        
+        float angle = Mathf.Atan(targetDirection.x/targetDirection.y) * Mathf.Rad2Deg;
+        if (targetDirection.y < 0)
+            angle = 180 + angle;
+        if (angle < 0)
+            angle +=  360;
+        transform.eulerAngles = new Vector3(0, 0, -angle);
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : BodyController {
+
+	void Update () {
+		float ver = Input.GetAxis("Vertical");
+		float hor = Input.GetAxis("Horizontal");
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        
+        Vector3 direction = (Vector3.up * ver + Vector3.right * hor) * speed * Time.deltaTime;
+        // transform.position += (transform.up * ver + transform.right * hor) * speed * Time.deltaTime;
+        // transform.position += (Vector3.up * ver + Vector3.right * hor) * speed * Time.deltaTime;
+        MoveTowards(direction);
+        // Debug.Log(direction);
+        
+        Vector3 targetDirection = mousePos - transform.position;
+        AimAt(targetDirection);
+	}
+}
