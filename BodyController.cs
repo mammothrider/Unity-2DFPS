bdc5e15dc8245252f6ca3e90586673cb1a2053af
Mammothrider <xushubin1992@gmail.com>
2017-09-08 19:41:17 +0900

    Draw trajectory using GL

--- BodyController.cs
+++ BodyController.cs
@@ -7,9 +7,15 @@ public class BodyController : MonoBehaviour {
     public float speed = 1;
     private Rigidbody2D selfRigidbody;
     private LineRenderer line;
+    private Weapon selfWeapon;
+    private const float selfRadius = 0.2f;
+    
+    private Queue<cTrajectory> drawQueue;
 	// Update is called once per frame
     void Awake() {
         selfRigidbody = GetComponent<Rigidbody2D>();
+        drawQueue = new Queue<cTrajectory>();
+        GetWeapon(GameObject.Find("Gun"));
     }
     
     protected void MoveTowards(Vector3 direction) {
@@ -30,7 +36,73 @@ public class BodyController : MonoBehaviour {
         transform.eulerAngles = new Vector3(0, 0, -angle);
     }
     
-    protected void Shoot() {
+    protected void GetWeapon(GameObject weapon) {
+        selfWeapon = weapon.GetComponent<Weapon>();
+    }
+    
+    protected void LoseWeapon() {
+        selfWeapon = null;
+    }
+    
+    protected cTrajectory Shoot() {
+        if (selfWeapon == null)
+            return null;
         
+        cTrajectory tmpTra = new cTrajectory();
+        //first shot
+        if (drawQueue.Count == 0) {
+            //self postion + radius
+            tmpTra.startPos = transform.position + transform.up * selfRadius;
+        }
+        else {
+            // + random start position around the gun
+            tmpTra.startPos = transform.position + transform.up * selfRadius + transform.right * Random.Range(-1f, 1f) * selfWeapon.startPrecision;
+        }
+        
+        tmpTra.endPos = tmpTra.startPos + transform.up * selfWeapon.attackRange + transform.right * Random.Range(-1f, 1f) * selfWeapon.endPrecision;
+        
+        tmpTra.time = selfWeapon.flyingTime;
+        
+        Debug.DrawLine(tmpTra.startPos, tmpTra.endPos, selfWeapon.fireMaterial.color);
+        drawQueue.Enqueue(tmpTra);
+        return tmpTra;
+    }
+    
+    public void OnRenderObject()
+    {
+        if (drawQueue.Count < 0)
+            return;
+        // Apply the line material
+        selfWeapon.fireMaterial.SetPass(0);
+        Color fireColor = selfWeapon.fireMaterial.color;
+
+        // GL.PushMatrix();
+        // Set transformation matrix for drawing to
+        // match our transform
+        // GL.MultMatrix(transform.localToWorldMatrix);
+
+        // Draw lines
+        GL.Begin(GL.LINES);
+        
+        foreach (cTrajectory t in drawQueue)
+        {
+            //alpha get lower when time pass by
+            GL.Color(new Color(fireColor.r, fireColor.g, fireColor.b, fireColor.a * t.time / selfWeapon.flyingTime));
+            GL.Vertex(t.startPos);
+            GL.Vertex(t.endPos);
+            // GL.Vertex(new Vector3(0, 0, 0));
+            // GL.Vertex(new Vector3(0, 1, 0));
+            
+            //decrease trajectory last time
+            t.time -= Time.deltaTime;
+        }
+        
+        //pop every trajectory that time is less than 0
+        while (drawQueue.Count > 0 && drawQueue.Peek().time < 0) {
+                drawQueue.Dequeue();
+        }
+        
+        GL.End();
+        // GL.PopMatrix();
     }
 }
---
+++ cTrajectory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cTrajectory{
+        public Vector3 startPos;
+        public Vector3 endPos;
+        public float time;
+}
