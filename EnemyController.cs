2026b9e0ae95c47968fcf7ac05d2a00a14c920c4
Mammothrider <xushubin1992@gmail.com>
2017-09-18 19:53:43 +0900

    敌人结构

--- EnemyController.cs
+++ EnemyController.cs
@@ -3,14 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : BodyController {
+    
+    public float raycastDegree = 2;
+    public float visionField = 2;
 
-	// Use this for initialization
-	void Start () {
-		
+	void FixedUpdate () {
+		float ver = Input.GetAxis("Vertical");
+		float hor = Input.GetAxis("Horizontal");
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        
+        Vector3 direction = (Vector3.up * ver + Vector3.right * hor) * speed * Time.deltaTime;
+        // transform.position += (transform.up * ver + transform.right * hor) * speed * Time.deltaTime;
+        // transform.position += (Vector3.up * ver + Vector3.right * hor) * speed * Time.deltaTime;
+        MoveTowards(direction);
+        // Debug.Log(direction);
+        
+        Vector3 targetDirection = mousePos - transform.position;
+        AimAt(targetDirection);
+        
+        if (Input.GetMouseButton(0))
+            Shoot();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    
+    //巡逻
+    //在固定点间来回移动
+    void Patrol() {} 
+    
+    //搜索
+    //寻找敌人，并准备攻击
+    void Search() {
+        Vector3 targetDirection;
+        while 
+    }
+    
+    //战斗
+    //攻击敌人，躲避敌人攻击
+    void Battle() {}
+    
+    //撤退
+    //远离战斗区域
+    void Retreat() {
+        
+    }
 }
