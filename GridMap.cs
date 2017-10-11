2c5cd1bd62b2209eb459cdf03ba571af5842b6c9
Mammothrider <xushubin1992@gmail.com>
2017-10-11 19:56:18 +0900

    pathfinding map

---
+++ GridMap.cs
@@ -0,0 +1,20 @@
+﻿
+//grid map
+//naive space tree
+//graph theory
+//graph flow
+public class GridMap{
+
+    Vector3 size;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
